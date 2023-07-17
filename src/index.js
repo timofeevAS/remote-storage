5d994283255c8d3100c1cc857831e560bcc32810
Александр <timofeev2.as@edu.spbstu.ru>
2023-07-17 10:53:44 +0300

    front-initial commit ver0.01

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'typeface-roboto';
+
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faDownload, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faDownload, faTrashCan);
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <App />
+
 );
+//root.render(
+//  <h1>Hello, World!</h1>
+//);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
