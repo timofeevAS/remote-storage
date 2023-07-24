87198305fa0fed4954ca292c625c3910286a3641
Александр <timofeev2.as@edu.spbstu.ru>
2023-07-24 14:37:57 +0300

    file load

--- myapp/client/src/FileForm.js
+++ myapp/client/src/FileForm.js
@@ -1,13 +1,54 @@
-import React from 'react';
-import { Form, Button, Container } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
 
 const FileUploadForm = () => {
-  return (
+
+  const [fileName, setFileName] = useState('');
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+
+    if (fileName.trim() === '') {
+      formData.append('name', selectedFile.name);
+    } else {
+      formData.append('name', fileName);
+    }
+
+    formData.append('file', selectedFile);
+
+    // Отправка запроса на сервер
+    fetch('http://localhost:8000/users/files/', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('File uploaded:', data);
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+return (
     <Container style={{ maxWidth: '20%' }}>
       <Form className="d-flex flex-column bd-highlight mb-3">
         <Form.Group controlId="formFileName">
           <Form.Label>Название файла</Form.Label>
-          <Form.Control type="text" placeholder="Введите название файла" />
+          <Form.Control
+            type="text"
+            placeholder="Введите название файла"
+            value={fileName}
+            onChange={(e) => setFileName(e.target.value)}
+          />
         </Form.Group>
 
         <Form.Group controlId="formFileType">
@@ -20,10 +61,10 @@ const FileUploadForm = () => {
 
         <Form.Group controlId="formFileUpload">
           <Form.Label>Выберите файл для загрузки</Form.Label>
-          <Form.Control type="file" />
+          <Form.Control type="file" onChange={handleFileChange} />
         </Form.Group>
 
-        <Button variant="primary" type="submit">
+        <Button variant="primary" type="submit" onClick={handleFormSubmit}>
           Загрузить
         </Button>
       </Form>
--- myapp/sender/views.py
+++ myapp/sender/views.py
@@ -6,11 +6,10 @@ from django.http import HttpResponseRedirect
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, serializers
 
 from django.contrib.auth.models import User
 
-from .serializers import FileSerializer
 
 def user_files(request):
     '''
@@ -51,28 +50,95 @@ def user_files(request):
     if 'success_message' in request.session:
         success_message = request.session.pop('success_message')
 
-    return render(request, 'files.html', {'success_message': success_message, 'files': my_files,'form':form})
+    return render(request, 'files.html', {'success_message': success_message, 'files': my_files, 'form': form})
+
+
+class FileListSerializer(serializers.ModelSerializer):
+    '''
+    Serializer for file listing
+    '''
+    extension = serializers.SerializerMethodField()
+    name = serializers.SerializerMethodField()
+    url = serializers.SerializerMethodField()
+
+    def get_extension(self, obj):
+        return obj.name.split('.')[-1] if '.' in obj.name else ''
+
+    def get_name(self, obj):
+        return obj.name.split('.')[0] if '.' in obj.name else ''
+
+    def get_url(self, obj):
+        return obj.file.url
+
+    class Meta:
+        model = MyFile
+        fields = ['id', 'name', 'extension', 'url', 'size', 'view_amount', 'created_at', 'owner']
+
+
+class FileUploadSerializer(serializers.ModelSerializer):
+    """
+    Upload file serializer
+    """
+
+    class Meta:
+        model = MyFile
+        fields = ['file', 'name']
+
+    def create(self, validated_data):
+        superuser = User.objects.get(is_superuser=True, username='admin')
+        name = validated_data.get('name')
+        if name == '':
+            if 'file' in validated_data:
+                name = validated_data['file'].name
+            else:
+                raise serializers.ValidationError('File name is required.')
+        else:
+            extension = validated_data['file'].name.split('.')[-1] if '.' in validated_data['file'].name else ''
+            name = validated_data.get('name') + '.' + extension
+
+
+        try:
+            my_file = MyFile.objects.create(
+                name=name,
+                file=validated_data['file'],
+                owner=superuser,
+                size=validated_data['file'].size,
+            )
+        except:
+            raise serializers.ValidationError('Can\'t create file')
+
+        return my_file
 
 
 class FileListView(APIView):
-    '''
-    GET method for filelist
-    '''
+    """
+    View for filelist
+    """
 
     def get(self, request):
         files = MyFile.objects.all()
-        serializer = FileSerializer(files, many=True)
+        serializer = FileListSerializer(files, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, *args, **kwargs):
+        serializer = FileUploadSerializer(data=request.data)
+        print(request.data['name'])
+        if serializer.is_valid():
+            my_file = serializer.save()
+            return Response({'message': 'File uploaded successfully.', 'file_id': my_file.id}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class FileDeleteView(APIView):
     '''
     FILE Delete view.
     '''
+
     def delete(self, request, pk):
         try:
             file = MyFile.objects.get(pk=pk)
             file.delete()
             return Response(status=status.HTTP_200_OK)
         except MyFile.DoesNotExist:
-            return Response(body={'error':'File not found'},status=status.HTTP_404_NOT_FOUND)
+            return Response(body={'error': 'File not found'}, status=status.HTTP_404_NOT_FOUND)
